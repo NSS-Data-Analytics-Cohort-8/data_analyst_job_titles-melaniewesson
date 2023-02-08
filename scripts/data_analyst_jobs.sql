cf5d0b76e3c8bc03df22e042aae0aebc04992507
Melanie Wesson <melaniedwesson@gmail.com>
2023-02-07 20:36:57 -0600

    finished question 12

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -23,7 +23,7 @@ SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location = 'TN'
 
--- 21
+-- 21 postings
 
 -- 4.	How many postings in Tennessee have a star rating above 4?
 
@@ -32,7 +32,7 @@ FROM data_analyst_jobs
 WHERE location = 'TN' 
 AND star_rating > 4;
 
--- 3
+-- 3 postings
 
 
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
@@ -41,26 +41,69 @@ SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
 
--- 151
+-- 151 postings
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
 SELECT location AS state, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
-GROUP BY location;
+WHERE star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC;
 
+-- Nebraska
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+
+-- 881 unique job titles
+
 -- 8.	How many unique job titles are there for California companies?
+
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
+
+-- 230 unique job titles
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count >5000 AND company IS NOT NULL
+GROUP BY company;
+
+-- 40 companies (and 1 NULL)
+
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count >5000 AND company IS NOT NULL
+GROUP BY company
+ORDER BY avg_rating DESC;
+
+-- Unilever, General Motors, Nike, American Express, Microsoft, and Kaiser Permanente all have average ratings of 
+4.199999809
 
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title LIKE '%Analyst%';
+
+-- 1636 job titles
+
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+SELECT title
+FROM data_analyst_jobs
+WHERE LOWER(title) NOT LIKE LOWER('%Analyst%')
+AND LOWER(title) NOT LIKE LOWER('%Analytics%');
+
+-- 4 job titles.  These jobs are all related to data visualization.
 
 -- **BONUS:**
 -- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
